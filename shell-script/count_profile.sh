5b5ab246e80100d673512e6ff02003361ab23dcd
Jay Wei <41401372+jayjayjohn@users.noreply.github.com>
2020-05-31 14:08:15 -0400

    Update count_profile.sh

--- shell-script/count_profile.sh
+++ shell-script/count_profile.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
+#dbTable,column
 #db.member,typ_cd
+
 input=$1
 
 set -x
 
 timeStamp=$(date '+%m%d%Y_%H%M')
-hostname='192.168.0.41'
+hostname='000.000.0.00'
 
 echo "profile_type,table_name,column_name,column_value,count" > profile_report.csv
 
@@ -28,7 +30,15 @@ union all
 select 'null_count' as profile, '$table' as table, '$column' as column,$column as value, count(1) as cnt from $dbTable where $column is null group by $column order by count(1) desc limit 5
 " > hiveql_profile_query_00584878.sql
 
-beeline -u "jdbs:hive2://$hostname:10000/default;principal=hive/$hostname@domain.com"\
---showHeader=false --silent=false --outputformat=dsv --delimiterForDSV=',' -f hiveql_profile_query_00584878.sql|sed -E '/^[[:space:]]*$/d' |sed -E 's/ +//g' > profile_result_$timeStamp.csv
+result=`beeline -u "jdbs:hive2://$hostname:10000/default;principal=hive/$hostname@domain.com hiveconf mapred.job.queue.name=root.hive"\--showHeader=false --silent=false --outputformat=dsv --delimiterForDSV=',' -f hiveql_profile_query_00584878.sql|sed -E '/^[[:space:]]*$/d' |sed -E 's/ +//g'`
+
+if [ "$result" == ""];
+then 
+	echo "invalid input line: $line"
+else 
+	echo $result >> profile_result_$timeStamp.csv
+fi
+
+
 
 done < "$input"

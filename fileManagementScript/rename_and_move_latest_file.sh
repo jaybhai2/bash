ba78c2e31ba5f2cfde806e1e50ecf18a16c3aa37
Jay Wei <41401372+jayjayjohn@users.noreply.github.com>
2020-06-12 10:35:46 -0400

    Update rename_and_move_latest_file.sh

--- fileManagementScript/rename_and_move_latest_file.sh
+++ fileManagementScript/rename_and_move_latest_file.sh
@@ -5,8 +5,14 @@ file_name_starting=log_response
 # list all matching file by modified date desc and fetch first one
 src_file=$(ls -t1 $path$file_name_starting* | head -n1)
 
+if [[ -z "$src_file" ]]
+then
+echo "file not found"
+else
 
 tgt_path=/home/ec2-user/staging/
+
 tgt_file=tgt.txt
 
 cp $src_file $tgt_path$tgt_file
+fi
